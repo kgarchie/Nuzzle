0819f678c5b7b751629b3b8d5780f8479b3cf744
kgarchie <archiethebig@gmail.com>
2023-06-06 18:11:07 +0300

    Basic features

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Nuzzle
 
-This is a set up wizard to help devs set up drizzle
+This simple CLI program helps devs get started with either a Nuxt + Drizzle or Next + Drizzle project
 
-We are working of features to set up Nuxt/Next with it. Kind of like Sidebase, with options to select the database(those supported by [drizzle](https://github.com/drizzle-team/drizzle-orm)), and automatically bootstrapping it to the project. Later on, we'll develop our custom ORM based on [Prisma](https://github.com/prisma/prisma) Syntax and Drizzle functionality, called [Prizzle](https://github.com/kgarchie/Prizzle.git)
-
-This project's aim is to make getting started with Drizzle a breeze. It is not yet complete.
-
-A demo exists for now using npx
+To get started, simply run the following command. The setup wizard will guide you through installation.
 ``` bash
 npx nuzzle
 ```
+
+This is a very early version, more features are being worked on. Version 1 will release soon
--- index.ts
+++ index.ts
@@ -1,40 +1,261 @@
 #!/usr/bin/env node
 import inquirer, { Answers } from 'inquirer';
+import { execSync } from 'child_process';
+import { exit } from 'process';
+import * as fs from 'fs';
 
-const questions = [
+const drizzleOrm = "drizzle-orm";
+const drizzleKit = "drizzle-kit"
+const defaultName = "my-app";
+
+enum Pm {
+    npm = "npm",
+    yarn = "yarn",
+    pnpm = "pnpm"
+}
+
+enum Database {
+    Sqlite = "Sqlite - Driver: better-sqlite3",
+    MySql = "MySql - Driver: mysql2",
+    Postgres = "Postgres - Driver: pg"
+}
+
+enum MetaFramework {
+    Nuxt = "nuxt",
+    Next = "next"
+}
+
+const databaseChoice = [
     {
         type: 'list',
         name: 'choice',
-        message: 'What would you like to do?',
+        message: 'Choose your preferred database engine:',
         choices: [
-            'Echo "Hello world"',
-            'Echo "Goodbye"',
-            'Enter your name to echo "Hello [name]"',
+            ...Object.values(Database)
+        ],
+    }
+];
+
+const sqliteConfigChoice = [
+    {
+        type: 'input',
+        name: 'database',
+        message: 'Enter your Sqlite database name:',
+        default: defaultName,
+        when: true
+    },
+];
+
+const configChoice = (db: string) => [
+    {
+        type: 'input',
+        name: 'host',
+        message: `Enter your ${db} host:`,
+        default: 'localhost',
+        when: true
+    },
+    {
+        type: 'input',
+        name: 'port',
+        message: `Enter your ${db} port:`,
+        default: db === 'mysql' ? '3306' : '5432',
+        when: true
+    },
+    {
+        type: 'input',
+        name: 'user',
+        message: `Enter your ${db} user:`,
+        default: db === 'mysql' ? 'root' : 'postgres',
+        when: true
+    },
+    {
+        type: 'password',
+        name: 'password',
+        message: `Enter your ${db} password:`,
+        default: '',
+        when: true
+    },
+    {
+        type: 'input',
+        name: 'database',
+        message: `Enter your ${db} database:`,
+        default: defaultName,
+        when: true
+    },
+    {
+        type: 'list',
+        name: 'pool',
+        message: `Do you want to use a connection pool for ${db}?`,
+        choices: [
+            'Yes',
+            'No'
+        ],
+        when: true
+    },
+    {
+        type: 'input',
+        name: 'poolMin',
+        message: `Enter your ${db} pool min:`,
+        default: '1',
+        when: (answers: Answers) => answers.pool === 'Yes'
+    },
+    {
+        type: 'input',
+        name: 'poolMax',
+        message: `Enter your ${db} pool max:`,
+        default: '5',
+        when: (answers: Answers) => answers.pool === 'Yes'
+    },
+];
+
+const metaFrameworkChoice = [
+    {
+        type: 'list',
+        name: 'choice',
+        message: 'Choose your preferred meta-framework:',
+        choices: [
+            ...Object.values(MetaFramework)
         ],
     },
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?',
-        when: function (answers: Answers) {
-            return answers.choice === 'Enter your name to echo "Hello [name]"';
-        },
-    },
+        message: 'Enter your project name:',
+        default: defaultName,
+        when: true
+    }
 ];
 
-inquirer.prompt(questions).then((answers) => {
-    switch (answers.choice) {
-        case 'Echo "Hello world"':
-            console.log('Hello world');
-            break;
-        case 'Echo "Goodbye"':
-            console.log('Goodbye');
-            break;
-        case 'Enter your name to echo "Hello [name]"':
-            console.log(`Hello ${answers.name}`);
-            break;
-        default:
-            console.log('Invalid choice');
-            break;
-    }
-});
+let pm = process.env.npm_execpath
+let database: Database;
+let projectName: string;
+
+
+if (pm?.includes('yarn') || pm?.includes('pnpm')) {
+    pm = pm.split('\\').pop()?.split('.').shift() as string;
+    pm = pm === 'yarn' ? Pm.yarn : Pm.pnpm;
+} else {
+    pm = Pm.npm;
+}
+console.log(`\x1b[33m${pm}\x1b[0m will be used to install dependencies and run the project.`);
+
+inquirer
+    .prompt(databaseChoice)
+    .then((answers: Answers) => {
+        switch (answers.choice) {
+            case Database.Sqlite:
+                database = Database.Sqlite;
+                break;
+            case Database.MySql:
+                database = Database.MySql;
+                break;
+            case Database.Postgres:
+                database = Database.Postgres;
+                break;
+            default:
+                console.log('No database selected. | This is an error Exiting...');
+                exit(1);
+        }
+
+        console.log(`\x1b[30m > ${database}\x1b[0m will be used as the database engine.`);
+
+        inquirer
+            .prompt(metaFrameworkChoice)
+            .then((metaFramework: Answers) => {
+                projectName = metaFramework.name.toLowerCase().replace(' ', '-');
+
+                switch (metaFramework.choice) {
+                    case MetaFramework.Nuxt:
+                        console.log(`\x1b[32m > ${MetaFramework.Nuxt}\x1b[0m chosen.`);
+                        console.log(`\x1b[30m > ${projectName}\x1b[0m will be the project name.`);
+                        try {
+                            execSync(`npx nuxi@latest init ${projectName}`, { stdio: 'inherit' });
+                        } catch (error: any) {
+                            console.error(error?.message);
+                        }
+                        break;
+                    case MetaFramework.Next:
+                        console.log(`\x1b[32m${MetaFramework.Next}\x1b[0m chosen.`);
+                        try {
+                            execSync(`npx create-next-app@latest ${projectName}`, { stdio: 'inherit' });
+                        } catch (error: any) {
+                            console.error(error?.message);
+                        }
+                        break;
+                    default:
+                        console.log('No meta-framework selected. | This is an error Exiting...');
+                        exit(1);
+                }
+
+                // install the database driver and dependencies
+                const databaseDriver = database.split(' - Driver: ').pop() as string;
+                const command = `cd ${projectName} && ${pm} ${pm === Pm.npm ? 'install' : 'add'} ${databaseDriver} ${drizzleOrm} ${drizzleKit} && ${pm} install`;
+
+                execSync(command, { stdio: 'inherit' });
+
+                function createConfigFile() {
+                    fs.writeFileSync(`${projectName}/db.ts`, configFile);
+
+                    // check if vscode is installed
+                    try {
+                        execSync('code --version', { stdio: 'ignore' });
+                    } catch (error) {
+                        console.log('Vscode is not installed. Exiting...');
+                        exit(0);
+                    }
+
+                    // open the project in vscode
+                    execSync(`code ${projectName}`, { stdio: 'inherit' });
+
+                    // print how to import the db with color
+                    console.log(`Here's how to import the db in your project:\n`);
+                    console.log(`\x1b[32mimport db from './db';\x1b[0m\n`);
+                }
+
+                // create the database config file
+                let configFile: string = '';
+                switch (database) {
+                    case Database.Sqlite:
+                        inquirer
+                            .prompt(sqliteConfigChoice)
+                            .then((sqliteConfig: Answers) => {
+                                configFile =  `import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
+                                import Database from 'better-sqlite3';
+                                 
+                                const sqlite = new Database('${sqliteConfig.name}');
+                                const db: BetterSQLite3Database = drizzle(sqlite);
+                                export default db;`;
+                                createConfigFile();
+                            });
+                        break;
+                    case Database.MySql:
+                        inquirer
+                            .prompt(configChoice("mysql"))
+                            .then((config: Answers) => {
+                                configFile = `import { drizzle } from "drizzle-orm/mysql2";
+                                import mysql from "mysql2/promise";
+                                
+                                const poolConnection = mysql.createPool({
+                                    host: '${config.host}',
+                                    user: '${config.user}',
+                                    password: '${config.password}',
+                                    database: '${config.database}',
+                                    waitForConnections: true,
+                                    connectionLimit: ${ config.poolMax },
+                                    queueLimit: ${ config.poolMin }
+                                });
+                                
+                                const db = drizzle(poolConnection);
+                                export default db; `;
+                                createConfigFile();
+                            });
+                        break;
+                    case Database.Postgres:
+                        console.log('Postgres config not implemented yet.');
+                        break;
+                    default:
+                        console.log('No database selected. | This is an error Exiting...');
+                        exit(1);
+                }
+            });
+    });
